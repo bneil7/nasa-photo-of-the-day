356f1448f22bedde434da4004ba05aebeaf5e6fa
Brandon Neil <brandonneil@Brandons-MacBook-Pro.local>
2020-06-11 00:16:30 -0600

    project meets mvp

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./App.css";
+import axios from 'axios'
+import {BASE_URL, API_KEY} from './Constants/index'
+import Header from './Components/Header'
+import Body from './Components/Body'
+import Footer from './Components/Footer'
 
 function App() {
+
+  const [apodData, setApodData] = useState([])
+
+  useEffect(() => {
+    console.log('🥇 ONLY AFTER FIRST RENDER')
+    axios
+      .get(`${BASE_URL}?api_key=${API_KEY}`)
+      .then(res => {
+       console.log('AW YEAH', res.data)
+       setApodData(res.data)
+      })
+      .catch(error => {
+      debugger
+      console.log('OH NO', error)
+      })
+  }, []);
+
   return (
     <div className="App">
+      <header>
+        <h1 className='title'>{apodData.title}</h1>
+        <h2 className='date'>{apodData.date}</h2>
+      </header>
+      <img className='img-section' src={apodData.url} alt="redundant word for pic goes here"></img>
+      <section className='explanation'>{apodData.explanation}</section>
       <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+        <span role="img" aria-label='go!'>🚀</span>!
       </p>
+      <footer className='copyright'>{apodData.copyright}</footer>
     </div>
   );
 }
---
+++ src/Constants/index.js
@@ -0,0 +1,2 @@
+export const BASE_URL = 'https://api.nasa.gov/planetary/apod'
+export const API_KEY = '7YWJOf6Sgpf6cwgLBgVLmP56v43TAvJGZGIpkU4P'
